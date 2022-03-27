95ea0a72add6f0d9a5577ca458f98c81bf123fde
filipeas <felipealvessampaio@hotmail.com>
2022-03-27 19:38:02 -0300

    implementacao de models para estados e cidades, juntamente com seeders para cada um #1

---
+++ api/ecoleta-api/app/Models/State.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+    use HasFactory;
+
+    protected $table = 'states';
+
+    protected $fillable = [
+        'code', 'name'
+    ];
+
+    public function city()
+    {
+        return $this->hasMany(City::class, 'uf', 'code');
+    }
+}
