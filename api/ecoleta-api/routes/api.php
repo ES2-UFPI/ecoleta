6e802b0441bda8c01425862fb893cca34d558d43
leonidasabreu16 <leonidasabreuti@gmail.com>
2022-04-04 21:03:40 -0300

    Finalização da Atividade #19
    Finalização da Atividade #19

--- api/ecoleta-api/routes/api.php
+++ api/ecoleta-api/routes/api.php
@@ -7,21 +7,11 @@ use App\Http\Controllers\RegionController;
 use App\Http\Controllers\StateController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::get('/city/{city}', [CityController::class, 'show']);
 Route::get('/state/{state}', [StateController::class, 'show']);
 
-Route::prefix('admin')->group(function () {//middleware(['auth:sanctum'])->
+//middleware(['auth:sanctum'])->
+Route::prefix('admin')->group(function () {
     // rotas da região
     Route::resource('region', RegionController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
     Route::get('/region/city/{city}', [RegionController::class, 'findRegionsByCity']);
