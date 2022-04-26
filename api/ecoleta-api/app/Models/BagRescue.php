0a7216244d8fd7c46a2cd366a16d953c0cf3c924
DevUnusual <carlos.meneses.g.s@gmail.com>
2022-04-26 09:39:53 -0300

    implementacao da atividade #43

--- api/ecoleta-api/app/Models/BagRescue.php
+++ api/ecoleta-api/app/Models/BagRescue.php
@@ -8,23 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class BagRescue extends Model
 {
     use HasFactory;
-
     // nome da tabela
     protected $table = 'bag_rescues';
 
     protected $fillable = [
         'company_id', 'bag_id', 'recue',
     ];
-
+    
+    // buscar sacola descartada vinculada
+    public function bag()
+    {
+        return $this->belongsTo(Bag::class, 'bag_id', 'id');
+    }
+    
     // buscar empresa
     public function company()
     {
         return $this->belongsTo(User::class, 'company_id', 'id');
     }
 
-    // buscar sacola descartada vinculada
-    public function bag()
-    {
-        return $this->belongsTo(Bag::class, 'bag_id', 'id');
-    }
 }
