a26eb06491c37d5680d3a500c86e310c6ed6cfb3
DevUnusual <carlos.meneses.g.s@gmail.com>
2022-05-10 09:03:59 -0300

    implementacao de atualizacao de telas #83

---
+++ ui/ecoleta/src/screens/ItensDeResgateFinalizados.js
@@ -0,0 +1,75 @@
+import React, { useState, Component } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Text, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class ItensDeResgateFinalizados extends Component {
+    constructor() {
+        super();
+    }
+
+    state = {
+        items: []
+    }
+
+    componentDidMount() {
+        this._unsubscribe = this.props.navigation.addListener('focus', () => {
+            console.log('Atualizando tela ItensDeResgateFinalizados');
+            const { items } = this.props.route.params;
+            this.setState({
+                items: items
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this._unsubscribe();
+    }
+
+    render() {
+
+        return (
+            <View style={styles.container} >
+                <Button
+                    style={{
+                        width: 60,
+                        alignSelf: 'stretch'
+                    }}
+                    icon={
+                        <Icon
+                            name='arrow-left'
+                            size={15}
+                            color='blue'
+                        />
+                    }
+                    onPress={() => this.props.navigation.goBack()}
+                />
+
+                <Text h3>Itens</Text>
+
+                <ScrollView>
+                    {this.state.items.map(item => (
+                        <View key={item.id}>
+                            <Text
+                                style={styles.item}
+                            >
+                                {item.collectionItem.title}
+                            </Text>
+                            <Text h6>Qtd.: {item.quantity}</Text>
+                        </View>
+                    ))
+                    }
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 10,
+        margin: 5,
+        fontSize: 20,
+        backgroundColor: '#eee',
+    },
+});
